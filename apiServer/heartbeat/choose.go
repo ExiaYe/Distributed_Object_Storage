43ecdd872a335024123b720507090814793f497f
exiaye <exiaye@outlook.com>
2024-09-08 18:39:39 +0800

    explain

--- apiServer/heartbeat/choose.go
+++ apiServer/heartbeat/choose.go
@@ -6,27 +6,40 @@ import (
 
 // 随机选择一个数据节点
 
+// ChooseRandomDataServers 函数用于从给定的数据服务器列表中随机选择n个服务器，并排除掉exclude列表中的服务器
 func ChooseRandomDataServers(n int, exclude map[int]string) (ds []string) {
+	// 创建一个空的候选服务器列表
 	candidates := make([]string, 0)
+	// 创建一个反向的exclude映射，用于快速查找
 	reverseExcludeMap := make(map[string]int)
+	// 遍历exclude映射，将地址和id存入reverseExcludeMap
 	for id, addr := range exclude {
 		reverseExcludeMap[addr] = id
 	}
+	// 获取所有数据服务器
 	servers := GetDataServers()
+	// 遍历所有数据服务器
 	for i := range servers {
 		s := servers[i]
+		// 检查当前服务器是否在exclude列表中
 		_, excluded := reverseExcludeMap[s]
+		// 如果不在exclude列表中，则将其加入候选服务器列表
 		if !excluded {
 			candidates = append(candidates, s)
 		}
 	}
+	// 获取候选服务器的数量
 	length := len(candidates)
+	// 如果候选服务器的数量小于n，则返回空列表
 	if length < n {
 		return
 	}
+	// 生成一个随机排列
 	p := rand.Perm(length)
+	// 遍历随机排列，将对应的候选服务器加入结果列表
 	for i := 0; i < n; i++ {
 		ds = append(ds, candidates[p[i]])
 	}
+	// 返回结果列表
 	return
 }
--- apiServer/heartbeat/heartbeat.go
+++ apiServer/heartbeat/heartbeat.go
@@ -13,47 +13,70 @@ import (
 var dataServers = make(map[string]time.Time)
 var mutex sync.Mutex
 
+// 监听心跳
 func ListenHeartbeat() {
+	// 创建rabbitmq连接
 	q := rabbitmq.New(rabbitmq.RABBITMQ_SERVER)
+	// 关闭连接
 	defer q.Close()
+	// 绑定队列
 	q.Bind("apiServers")
+	// 消费队列
 	c := q.Consume()
 	// 清除10s没收到心跳消息的数据节点
 	go removeExpiredDataServer()
+	// 循环消费队列
 	for msg := range c {
 		// 每个节点的监听地址
 		dataServer, e := strconv.Unquote(string(msg.Body))
 		if e != nil {
 			panic(e)
 		}
+		// 加锁
 		mutex.Lock()
+		// 更新数据节点
 		dataServers[dataServer] = time.Now()
+		// 解锁
 		mutex.Unlock()
 	}
 }
 
+// removeExpiredDataServer函数用于删除过期的数据服务器
 func removeExpiredDataServer() {
+	// 无限循环
 	for {
+		// 每隔5秒钟执行一次
 		time.Sleep(5 * time.Second)
+		// 加锁
 		mutex.Lock()
+		// 遍历dataServers
 		for s, t := range dataServers {
 			// 检查节点的最后一次心跳消息时间是否在10秒钟之前
 			if t.Add(10 * time.Second).Before(time.Now()) {
+				// 如果是，则删除该节点
 				delete(dataServers, s)
 			}
 		}
+		// 解锁
 		mutex.Unlock()
 	}
 }
 
 // 获取所有的数据节点监听地址
 
+// 获取数据服务器列表
 func GetDataServers() []string {
+	// 加锁
 	mutex.Lock()
+	// 在函数结束时解锁
 	defer mutex.Unlock()
+	// 创建一个空字符串切片
 	ds := make([]string, 0)
+	// 遍历dataServers
 	for s, _ := range dataServers {
+		// 将dataServers中的键添加到ds中
 		ds = append(ds, s)
 	}
+	// 返回ds
 	return ds
 }
