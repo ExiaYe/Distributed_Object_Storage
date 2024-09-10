434026239d0c4877f9389deaa919bb2db1b41004
exiaye <exiaye@outlook.com>
2024-09-10 23:40:37 +0800

    test

--- objectScanner/objectScanner.go
+++ objectScanner/objectScanner.go
@@ -19,21 +19,30 @@ func main() {
 	}
 }
 
+// 验证哈希
 func verify(hash string) {
+	// 打印验证哈希
 	log.Println("verify", hash)
+	// 获取哈希大小
 	size, e := es.SearchHashSize(hash)
+	// 如果获取哈希大小失败，打印错误信息
 	if e != nil {
 		log.Println(e)
 		return
 	}
+	// 获取哈希流
 	stream, e := objects.GetStream(hash, size)
+	// 如果获取哈希流失败，打印错误信息
 	if e != nil {
 		log.Println(e)
 		return
 	}
+	// 计算哈希
 	d := utils.CalculateHash(stream)
+	// 如果计算出的哈希与请求的哈希不匹配，打印错误信息
 	if d != hash {
 		log.Printf("object hash mismatch, calculated=%s, requested=%s", d, hash)
 	}
+	// 关闭哈希流
 	stream.Close()
 }
